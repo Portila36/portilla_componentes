3b785d6dcec0e20b66a7e7794a7c77080767f523
Portilla <jaredpor36@gmail.com>
2023-01-19 11:45:23 -0600

    portilla_componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import styled from "styled-components";
 import './App.css';
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Practica Componentes</h1>
+      <h2>De Jared Portilla Cardenas</h2>
+      <div>
+        <Button as="a" href="https://github.com/Portila36">Gifthub</Button>
+        <Button as="a" href="https://utd.edu.mx/" primary>UTD</Button>
+        <Button as="a" href="https://www.facebook.com/jared.portillacardenas">Facebook</Button>
+      </div>    
     </div>
   );
 }
